cacda11444471aea9b5ec330553473d4e72f964b
Laurence Lam <laurencelam@Laurences-MacBook-Pro.local>
2020-07-25 08:58:59 +0100

    added testing console

---
+++ UltimateDotNetTools.TestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UltimateDotNetTools.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
